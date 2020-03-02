47de0528374fc1957bc5eedd036ae5f3b2c0d938
dika aditia <aditiaaadicka@gmail.com>
2020-03-02 08:19:15 +0700

    create mutation and query for listing in graphql

--- src/graphql.ts
+++ src/graphql.ts
@@ -3,32 +3,36 @@ import {
   GraphQLObjectType,
   GraphQLString,
   GraphQLSchema,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLList
 } from "graphql";
 
-const listing = new GraphQLObjectType({
+import { listings } from "./listing";
+
+const Listing = new GraphQLObjectType({
   name: "Listing",
   fields: {
-    id: {
-      type: GraphQLID
-    },
-    title: { type: GraphQLString },
-    image: { type: GraphQLString },
-    address: { type: GraphQLString },
-    price: { type: GraphQLInt },
-    numOfGuests: { type: GraphQLInt },
-    numOfBeds: { type: GraphQLInt },
-    numOfBaths: { type: GraphQLInt },
-    rating: { type: GraphQLInt }
+    id: { type: GraphQLNonNull(GraphQLID) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    image: { type: GraphQLNonNull(GraphQLString) },
+    address: { type: GraphQLNonNull(GraphQLString) },
+    price: { type: GraphQLNonNull(GraphQLInt) },
+    numOfGuests: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBeds: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBaths: { type: GraphQLNonNull(GraphQLInt) },
+    rating: { type: GraphQLNonNull(GraphQLInt) }
   }
 });
 
 const query = new GraphQLObjectType({
   name: "Query",
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => "hello from query"
+    listings: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
+      resolve: () => {
+        return listings;
+      }
     }
   }
 });
@@ -36,7 +40,21 @@ const query = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
-    hello: { type: GraphQLString, resolve: () => "hello from mutation" }
+    deleteListing: {
+      type: GraphQLNonNull(Listing),
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) }
+      },
+      resolve: (_root, { id }) => {
+        for (let i = 0; i < listings.length; i++) {
+          if (listings[i].id == id) {
+            return listings.splice(i, 1)[0];
+          }
+        }
+
+        throw new Error("failed delete listing");
+      }
+    }
   }
 });
 
